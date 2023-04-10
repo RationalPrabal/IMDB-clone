670f60a5545c47533301e9250669bae0a3e6a9c7
Prabal <prabal19999@gmail.com>
2023-04-10 21:06:59 +0530

    creatad nav component

--- imdb/src/pages/index.js
+++ imdb/src/pages/index.js
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <h1 className='text-red-400'>
-    
+    <Navbar/>
     </h1>
   )
 }
