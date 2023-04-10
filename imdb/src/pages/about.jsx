570b694abf19a8d9a6b8dd043cc7d5a147f2edd2
Prabal <prabal19999@gmail.com>
2023-04-10 19:48:31 +0530

    added about page

---
+++ imdb/src/pages/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <div className='m-auto sm:w-1/2 text-center' >
+        <h1 className='text-2xl font-medium text-amber-600 my-2'>About</h1>
+        <h1>
+            This project is clone of popular movie database website named IMDB. This website provides comprehensive database of movies all around the world,along with latest news, ratings, reviews and trailors. 
+
+     
+        </h1>
+    </div>
+  )
+}
